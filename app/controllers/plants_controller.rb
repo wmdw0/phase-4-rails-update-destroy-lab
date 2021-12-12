2cc1989e100b5e07564018d22da419f5053ad6f4
William Walsh <wmdwalsh0@gmail.com>
2021-12-12 16:23:42 -0500

    commit tree 1cea78d0565e4e9338fef1cc8a790b8582b2ed10

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,9 +1,17 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
     plants = Plant.all
     render json: plants
+  end
+
+
+  # POST /plants
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
   end
 
   # GET /plants/:id
@@ -12,15 +20,39 @@ class PlantsController < ApplicationController
     render json: plant
   end
 
-  # POST /plants
-  def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
+  # PATCH /birds/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # PATCH /plants/:id/
+  def is_in_stock
+    plant = find_plant
+    plant.update(is_in_stock: !isInStock)
+    render json: plant
+  end
+
+  # DELETE /birds/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
 
+  def find_plant
+    Plant.find(params[:id])
+  end
+
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
 end
